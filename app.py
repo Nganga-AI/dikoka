0961cb77842c2180938c2be1aa62f73de544504b
eak <eak@ivalua.com>
2025-02-05 20:38:43 +0000

    Refactor RAG system initialization and streamline dataset loading by removing unused functions

--- app.py
+++ app.py
@@ -81,13 +81,12 @@ class ChatInterface:
 
 
 def get_rag_system(top_k_documents):
-    rag = RAGSystem(
-        "data/chroma_db", batch_size=64, top_k_documents=top_k_documents
-    )
+    rag = RAGSystem("data/chroma_db", batch_size=64, top_k_documents=top_k_documents)
     if not os.path.exists(rag.vector_store_management.persist_directory):
         documents = load_dataset()
         rag.initialize_vector_store(documents)
     return rag
+
 
 # Usage example:
 if __name__ == "__main__":
--- src/database.py
+++ src/database.py
@@ -1,21 +1,8 @@
 import json
-import os
 import random
 from glob import glob
 
-import tiktoken
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_core.documents import Document
-
-from .llm_evaluation.improve_generated_qa import (
-    parse_questions_answers_with_regex,
-)
-from .rag_pipeline.rag_system import RAGSystem
-
-
-def load_qa_dataset(folder_path: str):
-    qa_ds = parse_questions_answers_with_regex(folder_path)
-    return [(i[1], "QA_LLM") for i in qa_ds]
+from .vector_store.document_loader import load_dataset  # noqa
 
 
 def load_questions():
@@ -28,58 +15,3 @@ def load_final_summaries():
     data = [open(file).read() for file in files]
     random.shuffle(data)
     return data
-
-
-def load_summaries(folder_path: str):
-    files = sorted(glob(os.path.join(folder_path, "*.txt"))) + sorted(
-        glob(os.path.join(folder_path, "*/*.txt"))
-    )
-    summaries = [
-        (
-            open(path).read().strip(),
-            os.path.dirname(path) + "/" + os.path.basename(path),
-        )
-        for path in files
-    ]
-    return summaries
-
-
-def load_pages_from_folder(folder_path: str, chunk_size=512, chunk_overlap=100):
-    files = sorted(glob(os.path.join(folder_path, "*.txt")))
-    name = os.path.basename(folder_path)
-    pages = [open(path).read().strip() for path in files]
-    content = "\n\n".join(pages)
-    encoding = tiktoken.get_encoding("cl100k_base")
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size,
-        chunk_overlap=chunk_overlap,
-        length_function=lambda x: len(encoding.encode(x)),
-        separators=["\n\n", "\n", ". ", " ", ""],
-    )
-    summaries = text_splitter.split_text(content)
-    return [(i, name) for i in summaries]
-
-
-def load_pages_from_folders(folders_path: list[str], chunk_size=512, chunk_overlap=100):
-    data = sum(
-        [
-            load_pages_from_folder(folder, chunk_size, chunk_overlap)
-            for folder in folders_path
-        ],
-        start=[],
-    )
-    return data
-
-
-def load_dataset():
-    questions = load_qa_dataset("data/questions")
-    summaries = load_summaries("data/summaries")
-    pages = load_pages_from_folders(
-        ["data/297054", "data/297054_Volume_2"], chunk_size=512, chunk_overlap=100
-    )
-    documents = questions + summaries + pages
-    documents = [
-        Document(page_content=doc, metadata={"source": source})
-        for (doc, source) in documents
-    ]
-    return documents
